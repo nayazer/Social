9841722c2b7053f427d35ae6d22c9d368f0e8eff
MilAd <65576248+nayazer@users.noreply.github.com>
2020-09-22 23:07:19 +0330

    add help page and fixed some pages

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
+    path('help', views.help, name='help'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -12,3 +12,6 @@ def index(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def help(request):
+  return render(request, 'help.html')
